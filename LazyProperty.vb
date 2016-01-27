b5b25b2fbe1c618744cc83aeddd31ad3a56b117d
Nukepayload2 <1939357182@qq.com>
2016-01-27 18:16:46 +0800

    初始上传

---
+++ LazyProperty.vb
@@ -0,0 +1,10 @@
+﻿Public Module LazyProperty
+    Public Function SetValueIfEmpty(Of T As Class)(ByRef Varible As T, Calculate As Func(Of T)) As T
+        If Varible Is Nothing Then Varible = Calculate.Invoke
+        Return Varible
+    End Function
+    Public Function SetValueIfEmpty(Of T As Structure)(ByRef Varible As T?, Calculate As Func(Of T)) As T
+        If Varible Is Nothing Then Varible = Calculate.Invoke
+        Return Varible.Value
+    End Function
+End Module
---
+++ SettingViewModelExtension.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Reflection
+
+Module SettingViewModelExtension
+    Sub LoadProperties(Of T)(Target As T)
+        Dim sto = Windows.Storage.ApplicationData.Current.LocalSettings.Values
+        For Each prop In GetType(T).GetRuntimeProperties
+            If sto.ContainsKey(prop.Name) Then
+                prop.SetValue(Target, sto(prop.Name))
+            End If
+        Next
+    End Sub
+    Sub SaveProperties(Of T)(Target As T)
+        Dim sto = Windows.Storage.ApplicationData.Current.LocalSettings.Values
+        For Each prop In GetType(T).GetRuntimeProperties
+            Dim gotv = prop.GetValue(Target)
+            If gotv.GetType.ToString.Contains("CoreDispatcher") Then Continue For
+            If sto.ContainsKey(prop.Name) Then
+                sto(prop.Name) = gotv
+            Else
+                sto.Add(prop.Name, gotv)
+            End If
+        Next
+    End Sub
+    Sub SaveValue(Of T)(Name$, Value As T)
+        Dim sto = Windows.Storage.ApplicationData.Current.LocalSettings.Values
+        If sto.ContainsKey(Name) Then
+            sto(Name) = Value
+        Else
+            sto.Add(Name, Value)
+        End If
+    End Sub
+    Function LoadValue(Of T)(Name$) As T
+        Dim sto = Windows.Storage.ApplicationData.Current.LocalSettings.Values
+        If sto.ContainsKey(Name) Then
+            Dim v = sto(Name)
+            If TypeOf v Is T Then
+                Return DirectCast(v, T)
+            Else
+                Return Nothing
+            End If
+        End If
+        Return Nothing
+    End Function
+    Sub SaveCredential(ResName$, UserName$, Password$)
+        Dim vault = New Windows.Security.Credentials.PasswordVault()
+        Try
+            Dim cre = vault.FindAllByResource(ResName)
+            For Each c In cre
+                If c.UserName = UserName Then
+                    vault.Remove(c)
+                End If
+            Next
+        Catch ex As Exception
+        End Try
+        Try
+            If Not String.IsNullOrEmpty(Password) Then
+                vault.Add(New Windows.Security.Credentials.PasswordCredential(ResName, UserName, Password))
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+    Function LoadCredential$(ResName$, UserName$)
+        Dim vault = New Windows.Security.Credentials.PasswordVault()
+        Try
+            Return vault.Retrieve(ResName, UserName).Password
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+End Module
